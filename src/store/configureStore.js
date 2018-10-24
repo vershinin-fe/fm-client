49e80cdd2edabea057267403bb5821e8c3614cc8
vershinin-fe <few_w@mail.ru>
2018-10-24 15:28:37 +0500

    Add redux logging

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import rootReducer from '../reducers'
 
 const configureStore = preloadedState => createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, createLogger())
 );
 
 export default configureStore;
