ec67b15df9957a9c2b990583a734b92e59c521b8
vershinin-fe <few_w@mail.ru>
2018-11-11 20:01:29 +0500

    Add routes scratch

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 import ItemsListView from "../components/ItemsListView";
+import {Route, Switch} from "react-router-dom";
+import LoginForm from "../components/LoginForm";
 
 const App = ({ store }) => (
     <Provider store={store}>
-        <ItemsListView />
+        <div className="ui container">
+            <Switch>
+                <Route exact path="/" component={ItemsListView} />
+                <Route exact path="/login" component={LoginForm} />
+                <Route render={({ location }) => (
+                    <div className='ui inverted red segment'>
+                        <h3>
+                            Error! No matches for <code>{location.pathname}</code>
+                        </h3>
+                    </div>
+                )}/>
+            </Switch>
+        </div>
     </Provider>
 );
 
